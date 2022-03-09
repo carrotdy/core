a73499fecb1e55a103a6f70079fd5720e9b71601
DESKTOP-C55UU5R\홍다영 <dyhong9212@gmail.com>
2022-03-09 20:58:58 +0900

    22.03.09 새로운 할인 정책 개발/테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,7 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    @Override  //다형성에 의해 MemberRepository가 호출되는게 아니라 MemoryMemberRepository이 호출이 되어 save를 불러옴
+    @Override  //MemberRepository가 호출되는게 아니라 MemoryMemberRepository이 호출이 되어 save를 불러옴
     public void join(Member member) {
         memberRepository.save(member);
     }
