06eecf4932ae36eff0321f673481ab2295a40fbb
DESKTOP-C55UU5R\홍다영 <dyhong9212@gmail.com>
2022-03-16 23:12:34 +0900

    22.03.16 bean 중복등록 해결방법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -16,5 +19,10 @@ import org.springframework.context.annotation.FilterType;
  그래서 excludeFilters 를 이용해서 설정정보는 컴포넌트 스캔 대상에서 제외
  */
 
+//자동 빈 보다 수동 빈 등록이 우선권을 가진다.
 public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,6 +3,8 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성이 모두 자동으로 설정
+//@SpringBootApplication = @SpringBootConfiguration + @EnableAutoConfiguration + @ComponentScan
 @SpringBootApplication
 public class CoreApplication {
 
